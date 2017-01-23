b062616fb1b3c7249ce733f0895367a3f9c108f4
jhyang12345 <jhyang123494@gmail.com>
2017-01-23 15:44:02 +0900

    database attempt

---
+++ app/src/main/java/com/arrata/user/alarmapp/Alarm.java
@@ -0,0 +1,131 @@
+package com.arrata.user.alarmapp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by Jae Hyeong on 2017-01-23.
+ */
+
+public class Alarm extends RealmObject {
+
+    @PrimaryKey
+    private int code;
+
+    private int hours;
+    private int minutes;
+    private boolean monday = false;
+    private boolean tuesday = false;
+    private boolean wednesday = false;
+    private boolean thursday = false;
+    private boolean friday = false;
+    private boolean saturday = false;
+    private boolean sunday = false;
+    private boolean active;
+    public static String[] daysofweek = {"월", "화", "수", "목", "금", "토", "일"};
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+
+    public boolean isMonday() {
+        return monday;
+    }
+
+    public void setMonday(boolean monday) {
+        this.monday = monday;
+    }
+
+    public boolean isTuesday() {
+        return tuesday;
+    }
+
+    public void setTuesday(boolean tuesday) {
+        this.tuesday = tuesday;
+    }
+
+    public boolean isWednesday() {
+        return wednesday;
+    }
+
+    public void setWednesday(boolean wednesday) {
+        this.wednesday = wednesday;
+    }
+
+    public boolean isThursday() {
+        return thursday;
+    }
+
+    public void setThursday(boolean thursday) {
+        this.thursday = thursday;
+    }
+
+    public boolean isFriday() {
+        return friday;
+    }
+
+    public void setFriday(boolean friday) {
+        this.friday = friday;
+    }
+
+    public boolean isSaturday() {
+        return saturday;
+    }
+
+    public void setSaturday(boolean saturday) {
+        this.saturday = saturday;
+    }
+
+    public boolean isSunday() {
+        return sunday;
+    }
+
+    public void setSunday(boolean sunday) {
+        this.sunday = sunday;
+    }
+
+    public Alarm() {
+        hours = 0;
+        minutes = 0;
+        active = false;
+        monday = false;
+        tuesday = false;
+        wednesday = false;
+        thursday = false;
+        friday = false;
+        saturday = false;
+        sunday = false;
+    }
+}
--- app/src/main/java/com/arrata/user/alarmapp/MainActivity.java
+++ app/src/main/java/com/arrata/user/alarmapp/MainActivity.java
@@ -2,12 +2,48 @@ package com.arrata.user.alarmapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
+
+    Realm myRealm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myRealm = Realm.getInstance(this);
+
+        int startcode = 0;
+        int count = 0;
+        RealmResults<Alarm> results = myRealm.where(Alarm.class).findAll();
+        if(results.size() == 0) {
+            startcode = 0;
+        } else {
+            startcode = getNextKey();
+        }
+
+        myRealm.beginTransaction();
+
+        Alarm alarm1 = myRealm.createObject(Alarm.class);
+        alarm1.setCode(startcode);
+
+        myRealm.commitTransaction();
+
+        RealmResults<Alarm> results2 =
+                myRealm.where(Alarm.class)
+                        .findAll();
+        for(int i = 0; i < results2.size(); ++i) {
+            Log.d("Alarms", String.valueOf(results2.get(i).getHours()));
+        }
+        Log.d("Alarms", String.valueOf(results2.size()));
+    }
+
+    public int getNextKey() {
+        return myRealm.where(Alarm.class).max("code").intValue() + 1;
     }
 }
