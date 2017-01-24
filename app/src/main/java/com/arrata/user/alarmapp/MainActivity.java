0cb37fe10f7867bac386ead0367e06ab14774d6c
jhyang12345 <jhyang12345@naver.com>
2017-01-25 01:02:03 +0900

    first step of ui update

--- app/src/main/java/com/arrata/user/alarmapp/MainActivity.java
+++ app/src/main/java/com/arrata/user/alarmapp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.arrata.user.alarmapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
@@ -17,12 +20,23 @@ public class MainActivity extends AppCompatActivity {
 
     AlarmAdapter alarmAdapter;
 
+    ImageView addButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        addButton = (ImageView) findViewById(R.id.add_alarm);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
 
         LinearLayoutManager layoutManager =
                 new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
